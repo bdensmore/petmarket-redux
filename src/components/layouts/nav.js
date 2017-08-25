e59cb58d0cd3d3b12d8c170e49d159eb31b8b8f5
Ben <ben@densmore.net>
2017-08-25 17:13:17 -0400

    Add some styles for layouts

--- src/components/layouts/nav.js
+++ src/components/layouts/nav.js
@@ -1,23 +1,24 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-import Category from '../store/categories';
-
 class Nav extends Component {
   render() {
     return (
       <div className="row">
-        <image
-          src={require("../../assets/images/petmarketlogo_small.gif")}
-          alt="PetMarket Store"
-          width="182"
-          height="33" />
-        <div className="row gutters">
-          <Link to="/">home</Link>
-          <Link to="/">preferences</Link>
-          <Link to="/">about</Link>
-          <Link to="/">legal notice</Link>
-          <Link to="/">affiliate program</Link>
+        <div className="col col-4 push-left">
+          <img
+            className="img-class"
+            src={require("../../assets/images/petmarketlogo_small.gif")}
+            alt="PetMarket Store"
+            width="182"
+            height="33" />
+        </div>
+        <div className="col col-8 push-middle push-right links">
+            <Link to="/">home</Link>
+            <Link to="/">preferences</Link>
+            <Link to="/">about</Link>
+            <Link to="/">legal notice</Link>
+            <Link to="/">affiliate program</Link>
         </div>
       </div>
     )
